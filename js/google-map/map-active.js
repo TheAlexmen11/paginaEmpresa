31a39abc3831545d6ead0cc9a3fb61a98497a64b
Alex Michael <alexdcancho@gmail.com>
2021-06-29 23:08:30 -0500

    añadido todo la pagina con plantilla

---
+++ js/google-map/map-active.js
@@ -0,0 +1,40 @@
+var map;
+var latlng = new google.maps.LatLng(-16.401902, -71.529961);
+var stylez = [{
+    featureType: "all",
+    elementType: "all",
+    stylers: [{
+        saturation: -10
+            }]
+        }];
+var mapOptions = {
+    zoom: 50,
+    center: latlng,
+    scrollwheel: false,
+    scaleControl: false,
+    disableDefaultUI: true,
+    mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'gMap']
+    }
+};
+map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+var geocoder_map = new google.maps.Geocoder();
+var address = 'Compuplaza Arequipa';
+geocoder_map.geocode({
+    'address': address
+}, function (results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: map.getCenter()
+        });
+    } else {
+        //alert("Geocode was not successful for the following reason: " + status);
+    }
+});
+var mapType = new google.maps.StyledMapType(stylez, {
+    name: "Grayscale"
+});
+map.mapTypes.set('gMap', mapType);
+map.setMapTypeId('gMap');
